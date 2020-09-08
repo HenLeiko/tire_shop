1c31eac80693e64e2cc3d7fe0bcff13f000b7ab2
HenLeiko <jioiiika1337@gmail.com>
2020-09-08 18:57:45 +0300

    add router and routes

---
+++ app/config/db.php
@@ -0,0 +1,4 @@
+<?php
+require 'app/lib/rb.php';
+R::setup( 'mysql:host=localhost;dbname=tire_shop',
+    'root', 'root' );
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require 'app/config/db.php';
+
+
+function autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require @$fileName;
+}
+spl_autoload_register('autoload');
+
+$router = new \app\core\Router();
+$router->run();
